b4ac45a0a6f7f94d8eb92d60b65b1be6e8b5e41f
MANISH KUMAR <155035389+Manishkrthakur@users.noreply.github.com>
2025-05-10 16:12:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const buttons=document.querySelectorAll("button");
+const equal=document.querySelector(".btn");
+const input=document.getElementById("inputBox");
+
+let string ="";
+let arr=Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener("click",(e)=>{
+        if(e.target.innerHTML=='='){
+            string =eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            input.value=string;
+        }
+        else if(e.target.innerHTML=='DEL'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }else{
+        string += e.target.innerHTML;
+        input.value=string;
+        }
+    })
+})
